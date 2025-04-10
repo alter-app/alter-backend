8af005c619f8b28ef091c015e55f0c1845951d3a
Seungwan Yoo <ysw99@kakao.com>
2025-04-11 02:01:33 +0900

    init: initial commit

---
+++ src/main/java/com/dreamteam/alter/AlterApplication.java
@@ -0,0 +1,13 @@
+package com.dreamteam.alter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dreamteam/alter/AlterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dreamteam.alter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
