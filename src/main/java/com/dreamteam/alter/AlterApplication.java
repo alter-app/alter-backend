eb3af1b6b170142b2a0df7d3636cd731562f1ebe
Seungwan Yoo <ysw99@kakao.com>
2025-04-13 21:33:58 +0900

    chore: JpaAuditing 활성화

--- src/main/java/com/dreamteam/alter/AlterApplication.java
+++ src/main/java/com/dreamteam/alter/AlterApplication.java
@@ -2,8 +2,10 @@ package com.dreamteam.alter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class AlterApplication {
 
 	public static void main(String[] args) {
