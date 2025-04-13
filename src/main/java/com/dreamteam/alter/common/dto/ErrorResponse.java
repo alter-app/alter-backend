79154c3a18191a18b68468ce978c9b86e048725a
Seungwan Yoo <ysw99@kakao.com>
2025-04-13 18:34:32 +0900

    feat: Error 응답 DTO 구현

---
+++ src/main/java/com/dreamteam/alter/common/dto/ErrorResponse.java
@@ -0,0 +1,31 @@
+package com.dreamteam.alter.common.dto;
+
+import com.dreamteam.alter.common.exception.ErrorCode;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class ErrorResponse {
+
+    private String code;
+    private String message;
+
+    public static ErrorResponse of(ErrorCode errorCode) {
+        return new ErrorResponse(
+            errorCode.getCode(),
+            errorCode.getMessage()
+        );
+    }
+
+    public static ErrorResponse of(ErrorCode errorCode, String message) {
+        return new ErrorResponse(
+            errorCode.getCode(),
+            message
+        );
+    }
+
+}
