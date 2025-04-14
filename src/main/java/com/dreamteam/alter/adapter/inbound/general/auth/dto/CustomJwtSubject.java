d9a0a594daa80054147ed9209a4932512ac5e9d0
Seungwan Yoo <ysw99@kakao.com>
2025-04-14 15:12:52 +0900

    feat: JWT Subject용 VO 구현

---
+++ src/main/java/com/dreamteam/alter/adapter/inbound/general/auth/dto/CustomJwtSubject.java
@@ -0,0 +1,29 @@
+package com.dreamteam.alter.adapter.inbound.general.auth.dto;
+
+import com.dreamteam.alter.domain.auth.type.TokenScope;
+import com.dreamteam.alter.domain.auth.type.TokenType;
+import lombok.*;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class CustomJwtSubject {
+
+    private String authorizationId;
+
+    private TokenScope scope;
+
+    private TokenType type;
+
+    private String userId;
+
+    public static CustomJwtSubject of(
+        String authorizationId,
+        TokenScope scope,
+        TokenType type,
+        String userId
+    ) {
+        return new CustomJwtSubject(authorizationId, scope, type, userId);
+    }
+
+}
