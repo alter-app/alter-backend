07a1d651b21a4f4f3ab047f2c907903d4d3e5d2f
Seungwan Yoo <ysw99@kakao.com>
2025-04-16 11:26:26 +0900

    feat: 소셜 로그인 플랫폼 수집 정보 추가 - Kakao: name, birthyear, birthday, gender 수집 추가

--- src/main/java/com/dreamteam/alter/adapter/inbound/general/auth/dto/SocialUserInfo.java
+++ src/main/java/com/dreamteam/alter/adapter/inbound/general/auth/dto/SocialUserInfo.java
@@ -1,13 +1,14 @@
 package com.dreamteam.alter.adapter.inbound.general.auth.dto;
 
 import com.dreamteam.alter.domain.user.type.SocialProvider;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import com.dreamteam.alter.domain.user.type.UserGender;
+import lombok.*;
+import org.apache.commons.lang3.ObjectUtils;
 
 @Getter
-@NoArgsConstructor
-@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder(access = AccessLevel.PRIVATE)
 public class SocialUserInfo {
 
     private SocialProvider provider;
@@ -15,5 +16,50 @@ public class SocialUserInfo {
     private String socialId;
 
     private String email;
+
+    private String name;
+
+    private String birthday;
+
+    private UserGender gender;
+
+    public static SocialUserInfo of(
+        SocialProvider provider,
+        String socialId,
+        String email,
+        String name,
+        String birthyear,
+        String birthday,
+        String gender
+    ) {
+        return SocialUserInfo.builder()
+            .provider(provider)
+            .socialId(socialId)
+            .email(email)
+            .name(name)
+            .birthday(
+                ObjectUtils.isEmpty(birthyear) || ObjectUtils.isEmpty(birthyear) ?
+                    null : birthyear + birthday
+            )
+            .gender(
+                ObjectUtils.isNotEmpty(gender) ?
+                    gender.equals("male") ?
+                        UserGender.GENDER_MALE : UserGender.GENDER_FEMALE
+                    : null
+            )
+            .build();
+    }
+
+    public static SocialUserInfo of(
+        SocialProvider provider,
+        String socialId,
+        String email
+    ) {
+        return SocialUserInfo.builder()
+            .provider(provider)
+            .socialId(socialId)
+            .email(email)
+            .build();
+    }
 
 }
