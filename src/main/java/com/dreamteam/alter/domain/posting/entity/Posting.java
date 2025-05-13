d2d473dd16ed8ed7caaa0a5984dd6b7cc8d17fe8
Seungwan Yoo <ysw99@kakao.com>
2025-05-13 16:04:07 +0900

    feat: Posting(공고) 엔티티 구현, Posting Keyword, Schedule 엔티티 구현 및 연관관계 설정

---
+++ src/main/java/com/dreamteam/alter/domain/posting/entity/Posting.java
@@ -0,0 +1,92 @@
+package com.dreamteam.alter.domain.posting.entity;
+
+import com.dreamteam.alter.adapter.inbound.general.posting.dto.CreatePostingRequestDto;
+import com.dreamteam.alter.domain.posting.type.PaymentType;
+import com.dreamteam.alter.domain.posting.type.PostingStatus;
+import jakarta.persistence.*;
+import lombok.*;
+import org.apache.commons.lang3.ObjectUtils;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Table(name = "postings")
+@Builder(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@EntityListeners(AuditingEntityListener.class)
+public class Posting {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "workspace_id", nullable = false)
+    private Long workspace;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "description", nullable = false)
+    private String description;
+
+    @Column(name = "pay_amount", nullable = false)
+    private int payAmount;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "pay_type", nullable = false)
+    private PaymentType paymentType;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status", nullable = false)
+    private PostingStatus status;
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+
+    @OneToMany(mappedBy = "posting", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<PostingSchedule> schedules = new ArrayList<>();
+
+    @OneToMany(mappedBy = "posting", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<PostingKeyword> keywords = new ArrayList<>();
+
+    public static Posting create(CreatePostingRequestDto request) {
+        Posting posting = Posting.builder()
+            .workspace(request.getWorkspaceId())
+            .title(request.getTitle())
+            .description(request.getDescription())
+            .payAmount(request.getPayAmount())
+            .paymentType(request.getPaymentType())
+            .status(PostingStatus.OPEN)
+            .build();
+
+        if (ObjectUtils.isNotEmpty(request.getSchedules())) {
+            posting.schedules = request.getSchedules()
+                .stream()
+                .map(scheduleDto -> PostingSchedule.create(scheduleDto, posting))
+                .toList();
+        }
+
+        if (ObjectUtils.isNotEmpty(request.getKeywords())) {
+            posting.keywords = request.getKeywords()
+                .stream()
+                .map(keyword -> PostingKeyword.create(keyword, posting))
+                .toList();
+        }
+
+        return posting;
+    }
+
+}
