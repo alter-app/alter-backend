a6e564055844a3205b0b70c6e82b67efe832bf26
Seungwan Yoo <ysw99@kakao.com>
2025-06-06 23:34:58 +0900

    feat: id로 User 조회 구현

--- src/main/java/com/dreamteam/alter/adapter/outbound/user/persistence/UserQueryRepositoryImpl.java
+++ src/main/java/com/dreamteam/alter/adapter/outbound/user/persistence/UserQueryRepositoryImpl.java
@@ -6,13 +6,30 @@ import com.dreamteam.alter.domain.user.port.outbound.UserQueryRepository;
 import com.dreamteam.alter.domain.user.type.UserStatus;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.lang3.ObjectUtils;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 @RequiredArgsConstructor
 public class UserQueryRepositoryImpl implements UserQueryRepository {
 
     private final JPAQueryFactory queryFactory;
+
+    @Override
+    public Optional<User> findById(Long id) {
+        QUser qUser = QUser.user;
+        User user = queryFactory.select(qUser)
+            .from(qUser)
+            .where(
+                qUser.id.eq(id),
+                qUser.status.eq(UserStatus.ACTIVE)
+            )
+            .fetchOne();
+
+        return ObjectUtils.isEmpty(user) ? Optional.empty() : Optional.of(user);
+    }
 
     @Override
     public User findBySocialId(String socialId) {
