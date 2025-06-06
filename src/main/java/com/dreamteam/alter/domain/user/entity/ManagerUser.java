df66956ea6717002e9502e71bd6bdce01d6aa458
Seungwan Yoo <ysw99@kakao.com>
2025-06-06 23:46:46 +0900

    fix: User - ManagerUser 연관관계 수정 - 1:1 관계로 수정

--- src/main/java/com/dreamteam/alter/domain/user/entity/ManagerUser.java
+++ src/main/java/com/dreamteam/alter/domain/user/entity/ManagerUser.java
@@ -23,7 +23,7 @@ public class ManagerUser {
     private Long id;
 
     @JoinColumn(name = "user_id", nullable = false)
-    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @OneToOne(fetch = FetchType.LAZY, optional = false)
     private User user;
 
     @Enumerated(EnumType.STRING)
