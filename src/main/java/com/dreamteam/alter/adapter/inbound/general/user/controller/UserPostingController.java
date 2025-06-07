dc9139deac980e21051c1e153d2d62e0b7e2bd36
Seungwan Yoo <ysw99@kakao.com>
2025-06-08 00:41:55 +0900

    feat: [ALT-40] 공고 스크랩 관련 API 엔드포인트 구현

--- src/main/java/com/dreamteam/alter/adapter/inbound/general/user/controller/UserPostingController.java
+++ src/main/java/com/dreamteam/alter/adapter/inbound/general/user/controller/UserPostingController.java
@@ -1,14 +1,12 @@
 package com.dreamteam.alter.adapter.inbound.general.user.controller;
 
-import com.dreamteam.alter.adapter.inbound.common.dto.CommonApiResponse;
-import com.dreamteam.alter.adapter.inbound.common.dto.PageRequestDto;
-import com.dreamteam.alter.adapter.inbound.common.dto.PaginatedResponseDto;
+import com.dreamteam.alter.adapter.inbound.common.dto.*;
 import com.dreamteam.alter.adapter.inbound.general.posting.dto.UpdateUserPostingApplicationStatusRequestDto;
 import com.dreamteam.alter.adapter.inbound.general.posting.dto.UserPostingApplicationListResponseDto;
+import com.dreamteam.alter.adapter.inbound.general.user.dto.UserFavoritePostingListResponseDto;
 import com.dreamteam.alter.application.aop.AppActionContext;
 import com.dreamteam.alter.domain.user.context.AppActor;
-import com.dreamteam.alter.domain.user.port.inbound.GetUserPostingApplicationListUseCase;
-import com.dreamteam.alter.domain.user.port.inbound.UpdateUserPostingApplicationStatusUseCase;
+import com.dreamteam.alter.domain.user.port.inbound.*;
 import jakarta.annotation.Resource;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -17,7 +15,7 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/app/users/me/postings/applications")
+@RequestMapping("/app/users/me/postings")
 @PreAuthorize("hasAnyRole('USER', 'MANAGER', 'ADMIN')") // TODO: 권한 세부 설정
 @RequiredArgsConstructor
 @Validated
@@ -29,8 +27,17 @@ public class UserPostingController implements UserPostingControllerSpec {
     @Resource(name = "updateUserPostingApplicationStatus")
     private final UpdateUserPostingApplicationStatusUseCase updateUserPostingApplicationStatus;
 
+    @Resource(name = "createUserFavoritePosting")
+    private final CreateUserFavoritePostingUseCase createUserFavoritePosting;
+
+    @Resource(name = "getUserFavoritePostingList")
+    private final GetUserFavoritePostingListUseCase getUserFavoritePostingList;
+
+    @Resource(name = "deleteUserFavoritePosting")
+    private final DeleteUserFavoritePostingUseCase deleteUserFavoritePosting;
+
     @Override
-    @GetMapping
+    @GetMapping("/applications")
     public ResponseEntity<PaginatedResponseDto<UserPostingApplicationListResponseDto>> getMyPostingApplications(
         PageRequestDto pageRequest
     ) {
@@ -40,7 +47,7 @@ public class UserPostingController implements UserPostingControllerSpec {
     }
 
     @Override
-    @PatchMapping("/{applicationId}/status")
+    @PatchMapping("/applications/{applicationId}/status")
     public ResponseEntity<CommonApiResponse<Void>> updateMyPostingApplicationStatus(
         @PathVariable Long applicationId,
         UpdateUserPostingApplicationStatusRequestDto request
@@ -48,6 +55,34 @@ public class UserPostingController implements UserPostingControllerSpec {
         AppActor actor = AppActionContext.getInstance().getActor();
 
         updateUserPostingApplicationStatus.execute(actor, applicationId, request);
+        return ResponseEntity.ok(CommonApiResponse.empty());
+    }
+
+    @Override
+    @PostMapping("/favorites/{postingId}")
+    public ResponseEntity<CommonApiResponse<Void>> addUserFavoritePosting(@PathVariable Long postingId) {
+        AppActor actor = AppActionContext.getInstance().getActor();
+
+        createUserFavoritePosting.execute(actor, postingId);
+        return ResponseEntity.ok(CommonApiResponse.empty());
+    }
+
+    @Override
+    @GetMapping("/favorites")
+    public ResponseEntity<CursorPaginatedApiResponse<UserFavoritePostingListResponseDto>> getUserFavoritePostingList(
+        CursorPageRequestDto pageRequest
+    ) {
+        AppActor actor = AppActionContext.getInstance().getActor();
+
+        return ResponseEntity.ok(getUserFavoritePostingList.execute(actor, pageRequest));
+    }
+
+    @Override
+    @DeleteMapping("/favorites/{favoritePostingId}")
+    public ResponseEntity<CommonApiResponse<Void>> deleteUserFavoritePosting(@PathVariable Long favoritePostingId) {
+        AppActor actor = AppActionContext.getInstance().getActor();
+
+        deleteUserFavoritePosting.execute(actor, favoritePostingId);
         return ResponseEntity.ok(CommonApiResponse.empty());
     }
 
--- src/main/java/com/dreamteam/alter/adapter/inbound/general/user/controller/UserPostingControllerSpec.java
+++ src/main/java/com/dreamteam/alter/adapter/inbound/general/user/controller/UserPostingControllerSpec.java
@@ -1,11 +1,9 @@
 package com.dreamteam.alter.adapter.inbound.general.user.controller;
 
-import com.dreamteam.alter.adapter.inbound.common.dto.CommonApiResponse;
-import com.dreamteam.alter.adapter.inbound.common.dto.ErrorResponse;
-import com.dreamteam.alter.adapter.inbound.common.dto.PageRequestDto;
-import com.dreamteam.alter.adapter.inbound.common.dto.PaginatedResponseDto;
+import com.dreamteam.alter.adapter.inbound.common.dto.*;
 import com.dreamteam.alter.adapter.inbound.general.posting.dto.UpdateUserPostingApplicationStatusRequestDto;
 import com.dreamteam.alter.adapter.inbound.general.posting.dto.UserPostingApplicationListResponseDto;
+import com.dreamteam.alter.adapter.inbound.general.user.dto.UserFavoritePostingListResponseDto;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.ExampleObject;
@@ -19,7 +17,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@Tag(name = "APP - 사용자 지원한 공고 관련 API")
+@Tag(name = "APP - 사용자 공고 관련 API")
 public interface UserPostingControllerSpec {
 
     @Operation(summary = "사용자 공고 지원 목록 조회")
@@ -53,6 +51,52 @@ public interface UserPostingControllerSpec {
     ResponseEntity<CommonApiResponse<Void>> updateMyPostingApplicationStatus(
         @PathVariable @Min(1) Long applicationId,
         @RequestBody @Valid UpdateUserPostingApplicationStatusRequestDto request
+    );
+
+    @Operation(summary = "사용자 공고 스크랩 등록")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "사용자 공고 스크랩 등록 성공"),
+        @ApiResponse(responseCode = "400", description = "실패 케이스",
+            content = @Content(
+                mediaType = "application/json",
+                schema = @Schema(implementation = ErrorResponse.class),
+                examples = {
+                    @ExampleObject(
+                        name = "사용자가 이미 스크랩한 공고인 경우",
+                        value = "{\"code\" : \"B016\"}"
+                    ),
+                    @ExampleObject(
+                        name = "존재하지 않는 공고인 경우",
+                        value = "{\"code\" : \"B007\"}"
+                    )
+                }))
+    })
+    ResponseEntity<CommonApiResponse<Void>> addUserFavoritePosting(@PathVariable @Min(1) Long postingId);
+
+    @Operation(summary = "사용자 공고 스크랩 목록 조회 (커서 페이징)")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "사용자 공고 스크랩 목록 조회 성공")
+    })
+    ResponseEntity<CursorPaginatedApiResponse<UserFavoritePostingListResponseDto>> getUserFavoritePostingList(
+        CursorPageRequestDto pageRequest
+    );
+
+    @Operation(summary = "사용자 공고 스크랩 삭제")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "사용자 공고 스크랩 삭제 성공"),
+        @ApiResponse(responseCode = "400", description = "실패 케이스",
+            content = @Content(
+                mediaType = "application/json",
+                schema = @Schema(implementation = ErrorResponse.class),
+                examples = {
+                    @ExampleObject(
+                        name = "존재하지 않는 스크랩 공고인 경우",
+                        value = "{\"code\" : \"B015\"}"
+                    )
+                }))
+    })
+    ResponseEntity<CommonApiResponse<Void>> deleteUserFavoritePosting(
+        @PathVariable @Min(1) Long favoritePostingId
     );
 
 }
