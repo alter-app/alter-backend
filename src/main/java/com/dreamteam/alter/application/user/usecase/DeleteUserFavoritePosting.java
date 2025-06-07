523be14309764337003e7c325e522291f81e18ba
Seungwan Yoo <ysw99@kakao.com>
2025-06-08 00:41:18 +0900

    feat: [ALT-40] 사용자 공고 스크랩 삭제 API 구현

---
+++ src/main/java/com/dreamteam/alter/application/user/usecase/DeleteUserFavoritePosting.java
@@ -0,0 +1,28 @@
+package com.dreamteam.alter.application.user.usecase;
+
+import com.dreamteam.alter.common.exception.CustomException;
+import com.dreamteam.alter.common.exception.ErrorCode;
+import com.dreamteam.alter.domain.user.context.AppActor;
+import com.dreamteam.alter.domain.user.port.inbound.DeleteUserFavoritePostingUseCase;
+import com.dreamteam.alter.domain.user.port.outbound.UserFavoritePostingQueryRepository;
+import com.dreamteam.alter.domain.user.port.outbound.UserFavoritePostingRepository;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service("deleteUserFavoritePosting")
+@RequiredArgsConstructor
+@Transactional
+public class DeleteUserFavoritePosting implements DeleteUserFavoritePostingUseCase {
+
+    private final UserFavoritePostingQueryRepository userFavoritePostingQueryRepository;
+    private final UserFavoritePostingRepository userFavoritePostingRepository;
+
+    @Override
+    public void execute(AppActor actor, Long favoritePostingId) {
+        userFavoritePostingRepository.delete(
+            userFavoritePostingQueryRepository.findByIdAndUser(favoritePostingId, actor.getUser())
+                .orElseThrow(() -> new CustomException(ErrorCode.USER_FAVORITE_POSTING_NOT_FOUND)));
+    }
+
+}
