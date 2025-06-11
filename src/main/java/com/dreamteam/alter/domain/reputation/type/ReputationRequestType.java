dd201ab308fe0617e178fa9136edd53ad7323975
Seungwan Yoo <ysw99@kakao.com>
2025-06-11 23:48:54 +0900

    fix: ReputationRequest 관련 상태 및 타입 정의 수정

--- src/main/java/com/dreamteam/alter/domain/reputation/type/ReputationRequestType.java
+++ src/main/java/com/dreamteam/alter/domain/reputation/type/ReputationRequestType.java
@@ -1,7 +1,8 @@
 package com.dreamteam.alter.domain.reputation.type;
 
 public enum ReputationRequestType {
-    USER_TO_USER,
+    USER_TO_USER_INTERNAL,
+    USER_TO_USER_EXTERNAL,
     USER_TO_WORKSPACE,
     WORKSPACE_TO_USER
     ;
