0dec009c938016b8b9731536edddabb25fcc28f9
Seungwan Yoo <ysw99@kakao.com>
2025-06-14 23:24:39 +0900

    feat: 관리자 공고 지원서 상세 조회 UseCase 구현
    # Conflicts:
    #	src/main/java/com/dreamteam/alter/adapter/inbound/manager/posting/dto/PostingApplicationResponseApplicantSummaryDto.java

--- src/main/java/com/dreamteam/alter/adapter/inbound/manager/posting/dto/PostingApplicationResponseApplicantSummaryDto.java
+++ src/main/java/com/dreamteam/alter/adapter/inbound/manager/posting/dto/PostingApplicationResponseApplicantSummaryDto.java
@@ -8,7 +8,6 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 @Getter
