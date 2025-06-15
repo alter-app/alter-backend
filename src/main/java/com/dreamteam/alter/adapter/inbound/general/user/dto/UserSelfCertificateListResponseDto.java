18d4a5116326b15cef2b1c95bfaded8d0b12b392
Seungwan Yoo <ysw99@kakao.com>
2025-06-15 17:52:39 +0900

    hotfix: 사용자 자격사항 목록 조회 응답 스펙 임시 변경

--- src/main/java/com/dreamteam/alter/adapter/inbound/general/user/dto/UserSelfCertificateListResponseDto.java
+++ src/main/java/com/dreamteam/alter/adapter/inbound/general/user/dto/UserSelfCertificateListResponseDto.java
@@ -3,11 +3,13 @@ package com.dreamteam.alter.adapter.inbound.general.user.dto;
 import com.dreamteam.alter.domain.user.entity.UserCertificate;
 import com.dreamteam.alter.domain.user.type.CertificateType;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.annotation.Nullable;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
@@ -28,6 +30,10 @@ public class UserSelfCertificateListResponseDto {
     @Schema(description = "자격 이름", example = "정보처리기사")
     private String certificateName;
 
+    @Nullable
+    @Schema(description = "자격증 ID", example = "1234567890")
+    private String certificateId;
+
     @NotBlank
     @Schema(description = "발행기관", example = "한국산업인력공단")
     private String publisherName;
@@ -36,14 +42,25 @@ public class UserSelfCertificateListResponseDto {
     @Schema(description = "취득일", example = "2023-10-01")
     private LocalDate issuedAt;
 
+    @Nullable
+    @Schema(description = "만료일", example = "2024-10-01")
+    private LocalDate expiresAt;
+
+    @NotNull
+    @Schema(description = "최종 수정일", example = "2023-10-01T12:00:00")
+    private LocalDateTime updatedAt;
+
     public static UserSelfCertificateListResponseDto from(UserCertificate entity) {
         return UserSelfCertificateListResponseDto.builder()
-                .id(entity.getId())
-                .type(entity.getType())
-                .certificateName(entity.getCertificateName())
-                .publisherName(entity.getPublisherName())
-                .issuedAt(entity.getIssuedAt())
-                .build();
+            .id(entity.getId())
+            .type(entity.getType())
+            .certificateName(entity.getCertificateName())
+            .certificateId(entity.getCertificateId())
+            .publisherName(entity.getPublisherName())
+            .issuedAt(entity.getIssuedAt())
+            .expiresAt(entity.getExpiresAt())
+            .updatedAt(entity.getUpdatedAt())
+            .build();
     }
 
 }
