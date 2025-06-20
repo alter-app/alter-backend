5e03fb06db30cd3b17ec46b27fb5241192e4049e
Seungwan Yoo <ysw99@kakao.com>
2025-06-20 23:58:28 +0900

    fix: SocialRefreshToken 엔티티 객체 초기화 시 누락되었던 필드 추가

--- src/main/java/com/dreamteam/alter/adapter/outbound/auth/external/SocialRefreshTokenRepositoryImpl.java
+++ src/main/java/com/dreamteam/alter/adapter/outbound/auth/external/SocialRefreshTokenRepositoryImpl.java
@@ -2,6 +2,7 @@ package com.dreamteam.alter.adapter.outbound.auth.external;
 
 import com.dreamteam.alter.domain.auth.entity.SocialRefreshToken;
 import com.dreamteam.alter.domain.auth.port.outbound.SocialRefreshTokenRepository;
+import com.dreamteam.alter.domain.user.type.SocialProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,11 +15,11 @@ public class SocialRefreshTokenRepositoryImpl implements SocialRefreshTokenRepos
     private final SocialRefreshTokenJpaRepository socialRefreshTokenJpaRepository;
 
     @Override
-    public void saveOrUpdate(String socialId, String refreshToken) {
+    public void saveOrUpdate(SocialProvider provider, String socialId, String refreshToken) {
         socialRefreshTokenJpaRepository.findBySocialId(socialId)
             .ifPresentOrElse(
                 socialRefreshToken -> socialRefreshToken.update(refreshToken),
-                () -> socialRefreshTokenJpaRepository.save(SocialRefreshToken.create(socialId, refreshToken))
+                () -> socialRefreshTokenJpaRepository.save(SocialRefreshToken.create(provider, socialId, refreshToken))
             );
     }
 
--- src/main/java/com/dreamteam/alter/domain/auth/port/outbound/SocialRefreshTokenRepository.java
+++ src/main/java/com/dreamteam/alter/domain/auth/port/outbound/SocialRefreshTokenRepository.java
@@ -1,6 +1,8 @@
 package com.dreamteam.alter.domain.auth.port.outbound;
 
+import com.dreamteam.alter.domain.user.type.SocialProvider;
+
 public interface SocialRefreshTokenRepository {
-    void saveOrUpdate(String socialId, String refreshToken);
+    void saveOrUpdate(SocialProvider provider, String socialId, String refreshToken);
     void deleteBySocialId(String socialId);
 }
