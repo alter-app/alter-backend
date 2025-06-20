8e24bf9036155c9de08a09269e5ba30bd277199d
Seungwan Yoo <ysw99@kakao.com>
2025-06-20 23:59:25 +0900

    fix: 소셜 RefreshToken 저장 로직 공통 처리 - Kakao 로그인도 RefreshToken을 저장하도록 구성

--- src/main/java/com/dreamteam/alter/application/auth/service/AbstractSocialAuth.java
+++ src/main/java/com/dreamteam/alter/application/auth/service/AbstractSocialAuth.java
@@ -3,10 +3,17 @@ package com.dreamteam.alter.application.auth.service;
 import com.dreamteam.alter.adapter.inbound.general.auth.dto.SocialTokenResponseDto;
 import com.dreamteam.alter.adapter.inbound.general.auth.dto.SocialUserInfo;
 import com.dreamteam.alter.adapter.inbound.general.user.dto.LoginUserRequestDto;
+import com.dreamteam.alter.domain.auth.port.outbound.SocialRefreshTokenRepository;
 import com.dreamteam.alter.domain.user.type.SocialProvider;
 import org.apache.commons.lang3.ObjectUtils;
 
 public abstract class AbstractSocialAuth {
+
+    protected final SocialRefreshTokenRepository socialRefreshTokenRepository;
+
+    protected AbstractSocialAuth(SocialRefreshTokenRepository socialRefreshTokenRepository) {
+        this.socialRefreshTokenRepository = socialRefreshTokenRepository;
+    }
 
     public SocialUserInfo authenticate(LoginUserRequestDto request) {
         SocialTokenResponseDto socialTokens = getToken(request);
@@ -28,5 +35,9 @@ public abstract class AbstractSocialAuth {
     protected abstract SocialUserInfo getUserInfo(SocialTokenResponseDto socialTokens);
 
     public abstract boolean supports(SocialProvider provider);
+
+    protected void saveOrUpdateRefreshToken(SocialProvider provider, String socialId, String refreshToken) {
+        socialRefreshTokenRepository.saveOrUpdate(provider, socialId, refreshToken);
+    }
 
 }
