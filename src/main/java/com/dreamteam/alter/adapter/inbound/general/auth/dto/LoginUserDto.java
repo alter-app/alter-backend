7e303ace815472a4e09cacedcceaf6cacf4ca756
Seungwan Yoo <ysw99@kakao.com>
2025-10-22 23:37:28 +0900

    feat: 로그인 API 응답 스펙 변경

--- src/main/java/com/dreamteam/alter/adapter/inbound/general/auth/dto/LoginUserDto.java
+++ src/main/java/com/dreamteam/alter/adapter/inbound/general/auth/dto/LoginUserDto.java
@@ -2,7 +2,6 @@ package com.dreamteam.alter.adapter.inbound.general.auth.dto;
 
 import com.dreamteam.alter.domain.auth.entity.Authorization;
 import com.dreamteam.alter.domain.auth.type.TokenScope;
-import com.dreamteam.alter.domain.user.entity.User;
 import com.dreamteam.alter.domain.user.type.UserRole;
 import lombok.*;
 
@@ -11,6 +10,8 @@ import lombok.*;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class LoginUserDto {
+
+    private String authorizationId;
 
     private TokenScope scope;
 
@@ -22,6 +23,7 @@ public class LoginUserDto {
 
     public static LoginUserDto of(Authorization authorization, UserRole role) {
         return LoginUserDto.builder()
+            .authorizationId(authorization.getId())
             .scope(authorization.getScope())
             .id(authorization.getUser().getId())
             .email(authorization.getUser().getEmail())
