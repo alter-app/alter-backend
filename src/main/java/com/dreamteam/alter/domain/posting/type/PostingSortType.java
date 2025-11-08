8ed0d59cc98537502131667395ebcd26276f1eb8
Seungwan Yoo <ysw99@kakao.com>
2025-11-08 18:40:17 +0900

    feat: [alt-109] 지도 기준 공고 목록 조회 API 필터링 고도화
    - 키워드 검색 지원
    - 정렬 기준(최신순, 급여순) 조회 지원

--- src/main/java/com/dreamteam/alter/domain/posting/type/PostingSortType.java
+++ src/main/java/com/dreamteam/alter/domain/posting/type/PostingSortType.java
@@ -8,12 +8,14 @@ import java.util.Map;
 @Getter
 @RequiredArgsConstructor
 public enum PostingSortType {
-    PAY_AMOUNT
+    PAY_AMOUNT,
+    LATEST
     ;
 
     public static Map<PostingSortType, String> describe() {
         return Map.of(
-            PostingSortType.PAY_AMOUNT, "급여순"
+            PostingSortType.PAY_AMOUNT, "급여순",
+            PostingSortType.LATEST, "최신 등록 순"
         );
     }
 }
