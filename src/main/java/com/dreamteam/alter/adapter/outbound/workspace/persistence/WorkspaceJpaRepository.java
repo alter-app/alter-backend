e8d63c8815d7e5d1c768789b48ac2d40b5ab484a
Choi Junyoung <juny0955@gmail.com>
2026-01-12 20:45:17 +0900

    fix: 불필요한 메서드 삭제

--- src/main/java/com/dreamteam/alter/adapter/outbound/workspace/persistence/WorkspaceJpaRepository.java
+++ src/main/java/com/dreamteam/alter/adapter/outbound/workspace/persistence/WorkspaceJpaRepository.java
@@ -1,14 +1,8 @@
 package com.dreamteam.alter.adapter.outbound.workspace.persistence;
 
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.dreamteam.alter.domain.user.entity.ManagerUser;
 import com.dreamteam.alter.domain.workspace.entity.Workspace;
 
 public interface WorkspaceJpaRepository extends JpaRepository<Workspace, Long> {
-	Optional<Workspace> findByIdAndManagerUser(Long id, ManagerUser managerUser);
-
-	boolean existsByIdAndManagerUser(Long id, ManagerUser managerUser);
 }
