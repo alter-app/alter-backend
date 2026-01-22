e57b1a7571338814aa296492b4f01285b7f8fdd2
hodoon <ehgns79513@gmial.com>
2026-01-22 10:13:14 +0900

    refactor: 사용하지 않는 클래스 제거

--- src/main/java/com/dreamteam/alter/domain/workspace/port/inbound/GetMyMonthlyScheduleUseCase.java
+++
@@ -1,9 +0,0 @@
-package com.dreamteam.alter.domain.workspace.port.inbound;
-
-import com.dreamteam.alter.adapter.inbound.general.schedule.dto.MonthlyWorkScheduleInquiryRequestDto;
-import com.dreamteam.alter.adapter.inbound.general.schedule.dto.MyScheduleMonthlyResponseDto;
-import com.dreamteam.alter.domain.user.context.AppActor;
-
-public interface GetMyMonthlyScheduleUseCase {
-    MyScheduleMonthlyResponseDto execute(AppActor actor, MonthlyWorkScheduleInquiryRequestDto request);
-}
