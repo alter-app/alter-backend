b2869f1e305f9f1dadb454eae97e67a6c7013606
Seungwan Yoo <ysw99@kakao.com>
2026-02-27 16:34:36 +0900

    refactor: describe() 호출마다 Map 생성되는 비효율성 개선

--- src/main/java/com/dreamteam/alter/domain/user/type/UserGender.java
+++ src/main/java/com/dreamteam/alter/domain/user/type/UserGender.java
@@ -1,16 +1,25 @@
 package com.dreamteam.alter.domain.user.type;
 
-import java.util.Map;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 
+import java.util.Arrays;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Getter
+@AllArgsConstructor
 public enum UserGender {
-    GENDER_MALE,
-    GENDER_FEMALE
+    GENDER_MALE("남성"),
+    GENDER_FEMALE("여성")
     ;
 
+    private final String description;
+
+    private static final Map<UserGender, String> DESCRIPTION_MAP = Arrays.stream(values())
+        .collect(Collectors.toUnmodifiableMap(g -> g, UserGender::getDescription));
+
     public static Map<UserGender, String> describe() {
-        return Map.of(
-            UserGender.GENDER_MALE, "남성",
-            UserGender.GENDER_FEMALE, "여성"
-        );
+        return DESCRIPTION_MAP;
     }
 }
