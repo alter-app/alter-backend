d7efd5669ad8d0acd0a3f511bf1a5be004a625d7
hodoon <ehgns79513@gmial.com>
2026-02-27 11:24:34 +0900

    fix: EmailSendLog와 DB 타입 일치

--- src/main/java/com/dreamteam/alter/domain/email/entity/EmailSendLog.java
+++ src/main/java/com/dreamteam/alter/domain/email/entity/EmailSendLog.java
@@ -6,6 +6,8 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.JdbcType;
+import org.hibernate.dialect.PostgreSQLEnumJdbcType;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -30,7 +32,8 @@ public class EmailSendLog {
     private String email;
 
     @Enumerated(EnumType.STRING)
-    @Column(name = "status", nullable = false, length = 20)
+    @JdbcType(PostgreSQLEnumJdbcType.class)
+    @Column(name = "status", nullable = false)
     private EmailSendStatus status;
 
     @CreatedDate
