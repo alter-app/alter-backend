c79501d010725fa8ef35df491f7633f7e16053b0
Choi Junyoung <juny0955@gmail.com>
2026-03-21 16:08:04 +0900

    refactor: 댓글 길이 제한 추가

--- src/main/java/com/dreamteam/alter/adapter/inbound/manager/workspace/dto/CreateWorkspaceReasonCommentRequestDto.java
+++ src/main/java/com/dreamteam/alter/adapter/inbound/manager/workspace/dto/CreateWorkspaceReasonCommentRequestDto.java
@@ -1,6 +1,7 @@
 package com.dreamteam.alter.adapter.inbound.manager.workspace.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -13,6 +14,7 @@ import lombok.NoArgsConstructor;
 public class CreateWorkspaceReasonCommentRequestDto {
 
 	@Schema(description = "댓글 내용", example = "왜 반려죠")
+	@Max(255)
 	@NotBlank
 	private String comment;
 }
