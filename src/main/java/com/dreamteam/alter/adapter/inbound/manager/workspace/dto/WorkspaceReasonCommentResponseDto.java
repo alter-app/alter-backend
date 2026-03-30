fe16fabc26a4327cd0316d97f2dc2c1bc934c8a0
Choi Junyoung <juny0955@gmail.com>
2026-03-30 21:03:23 +0900

    refactor: api 응답 스키마 추가

--- src/main/java/com/dreamteam/alter/adapter/inbound/manager/workspace/dto/WorkspaceReasonCommentResponseDto.java
+++ src/main/java/com/dreamteam/alter/adapter/inbound/manager/workspace/dto/WorkspaceReasonCommentResponseDto.java
@@ -17,8 +17,14 @@ import lombok.NoArgsConstructor;
 @Builder(access = AccessLevel.PRIVATE)
 @Schema(description = "업장 등록 요청 승인/반려 댓글 조회 DTO")
 public class WorkspaceReasonCommentResponseDto {
+
+	@Schema(description = "댓글 ID", example = "1")
 	private Long id;
+
+	@Schema(description = "댓글 내용", example = "자료 누락")
 	private String comment;
+
+	@Schema(description = "댓글 생성시각", example = "2026-03-01T10:00:00")
 	private LocalDateTime createdAt;
 
 	public static WorkspaceReasonCommentResponseDto from(WorkspaceReasonComment entity) {
