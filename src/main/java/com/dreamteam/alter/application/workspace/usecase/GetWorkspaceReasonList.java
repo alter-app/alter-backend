ef1a29e2a91b9638fa3d531136f0d55c111b99ae
Choi Junyoung <juny0955@gmail.com>
2026-04-03 23:31:25 +0900

    feat: 유저 반려 사유 목록 조회 API

---
+++ src/main/java/com/dreamteam/alter/application/workspace/usecase/GetWorkspaceReasonList.java
@@ -0,0 +1,37 @@
+package com.dreamteam.alter.application.workspace.usecase;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.dreamteam.alter.adapter.inbound.general.workspace.dto.WorkspaceReasonResponseDto;
+import com.dreamteam.alter.common.exception.CustomException;
+import com.dreamteam.alter.common.exception.ErrorCode;
+import com.dreamteam.alter.domain.user.context.AppActor;
+import com.dreamteam.alter.domain.workspace.port.inbound.GetWorkspaceReasonListUseCase;
+import com.dreamteam.alter.domain.workspace.port.outbound.WorkspaceReasonQueryRepository;
+import com.dreamteam.alter.domain.workspace.port.outbound.WorkspaceRequestQueryRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service("getWorkspaceReasonList")
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class GetWorkspaceReasonList implements GetWorkspaceReasonListUseCase {
+
+	private final WorkspaceRequestQueryRepository workspaceRequestQueryRepository;
+	private final WorkspaceReasonQueryRepository workspaceReasonQueryRepository;
+
+	@Override
+	public List<WorkspaceReasonResponseDto> execute(AppActor actor, Long workspaceRequestId) {
+		if (!workspaceRequestQueryRepository.existsByIdAndUserId(workspaceRequestId, actor.getUserId())) {
+			throw new CustomException(ErrorCode.FORBIDDEN);
+		}
+
+		return workspaceReasonQueryRepository.getWorkspaceReasonList(workspaceRequestId)
+			.stream()
+			.map(WorkspaceReasonResponseDto::from)
+			.toList();
+	}
+}
