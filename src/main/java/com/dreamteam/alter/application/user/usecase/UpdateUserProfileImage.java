5bfc61defa056180279bd7bce4ec5bf5a9475583
Choi Junyoung <juny0955@gmail.com>
2026-05-03 14:31:11 +0900

    feat: 프로필 이미지 수정 usecase

---
+++ src/main/java/com/dreamteam/alter/application/user/usecase/UpdateUserProfileImage.java
@@ -0,0 +1,41 @@
+package com.dreamteam.alter.application.user.usecase;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.dreamteam.alter.common.exception.CustomException;
+import com.dreamteam.alter.common.exception.ErrorCode;
+import com.dreamteam.alter.domain.file.entity.File;
+import com.dreamteam.alter.domain.file.port.inbound.AttachFilesUseCase;
+import com.dreamteam.alter.domain.file.port.outbound.FileQueryRepository;
+import com.dreamteam.alter.domain.file.type.FileTargetType;
+import com.dreamteam.alter.domain.user.entity.User;
+import com.dreamteam.alter.domain.user.port.inbound.UpdateUserProfileImageUseCase;
+
+import lombok.RequiredArgsConstructor;
+
+@Service("updateUserProfileImage")
+@RequiredArgsConstructor
+@Transactional
+public class UpdateUserProfileImage implements UpdateUserProfileImageUseCase {
+
+	private final FileQueryRepository fileQueryRepository;
+	private final AttachFilesUseCase attachFiles;
+
+	@Override
+	public void execute(User user, String fileId) {
+		String targetId = user.getId().toString();
+		File file = fileQueryRepository.findByTargetTypeAndTargetId(FileTargetType.USER_PROFILE, targetId)
+			.orElseThrow(() -> new CustomException(ErrorCode.FILE_NOT_FOUND));
+		file.markDeleted();
+
+		attachFiles.execute(
+			List.of(fileId),
+			FileTargetType.USER_PROFILE,
+			targetId,
+			user.getId()
+		);
+	}
+}
