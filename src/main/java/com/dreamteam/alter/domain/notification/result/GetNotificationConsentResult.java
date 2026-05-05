2cc290a265d3cfe9a0b9d1398f0fb9cdf12b20d3
Seungwan Yoo <ysw99@kakao.com>
2026-05-05 10:31:19 +0900

    refactor: Result 클래스가 반환하는 Map의 불변성 보장

--- src/main/java/com/dreamteam/alter/domain/notification/result/GetNotificationConsentResult.java
+++ src/main/java/com/dreamteam/alter/domain/notification/result/GetNotificationConsentResult.java
@@ -3,16 +3,15 @@ package com.dreamteam.alter.domain.notification.result;
 import com.dreamteam.alter.domain.notification.entity.NotificationConsent;
 import com.dreamteam.alter.domain.notification.type.NotificationConsentType;
 
-import java.util.EnumMap;
 import java.util.Map;
 
 public record GetNotificationConsentResult(
     Map<NotificationConsentType, Boolean> consents
 ) {
     public static GetNotificationConsentResult from(NotificationConsent consent) {
-        Map<NotificationConsentType, Boolean> consents = new EnumMap<>(NotificationConsentType.class);
-        consents.put(NotificationConsentType.GENERAL, consent.isNotificationConsent());
-        consents.put(NotificationConsentType.NIGHT, consent.isNightNotificationConsent());
-        return new GetNotificationConsentResult(consents);
+        return new GetNotificationConsentResult(Map.of(
+            NotificationConsentType.GENERAL, consent.isNotificationConsent(),
+            NotificationConsentType.NIGHT, consent.isNightNotificationConsent()
+        ));
     }
 }
