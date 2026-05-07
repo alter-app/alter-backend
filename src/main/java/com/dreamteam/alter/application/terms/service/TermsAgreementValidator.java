52b66bb4af3c59300780d08de565271b0ef89279
hodoon <ehgns79513@gmial.com>
2026-05-07 17:44:05 +0900

    refactor: TermsAgreementValidator에 @Transactional(readOnly = true) 추가

--- src/main/java/com/dreamteam/alter/application/terms/service/TermsAgreementValidator.java
+++ src/main/java/com/dreamteam/alter/application/terms/service/TermsAgreementValidator.java
@@ -7,6 +7,7 @@ import com.dreamteam.alter.domain.terms.port.outbound.TermsQueryRepository;
 import com.dreamteam.alter.domain.terms.type.TermsType;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Set;
@@ -17,6 +18,7 @@ public class TermsAgreementValidator {
 
     private final TermsQueryRepository termsQueryRepository;
 
+    @Transactional(readOnly = true)
     public List<Terms> validateAndResolve(Set<TermsType> agreedTermsTypes) {
         List<Terms> latestPublishedTerms = termsQueryRepository.findLatestPublishedPerType();
 
