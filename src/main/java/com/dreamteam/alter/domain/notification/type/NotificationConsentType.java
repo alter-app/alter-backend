182f6fef875c1c42caec1e26734674e1c189550f
Seungwan Yoo <ysw99@kakao.com>
2026-05-21 00:38:59 +0900

    feat: NotificationConsentType에 SUBSTITUTE, REPUTATION 추가

--- src/main/java/com/dreamteam/alter/domain/notification/type/NotificationConsentType.java
+++ src/main/java/com/dreamteam/alter/domain/notification/type/NotificationConsentType.java
@@ -5,12 +5,16 @@ import java.util.Map;
 public enum NotificationConsentType {
     GENERAL,
     NIGHT,
+    SUBSTITUTE,
+    REPUTATION,
     ;
 
     public static Map<NotificationConsentType, String> describe() {
         return Map.of(
             NotificationConsentType.GENERAL, "전체 알림 수신",
-            NotificationConsentType.NIGHT, "야간 알림 수신"
+            NotificationConsentType.NIGHT, "야간 알림 수신",
+            NotificationConsentType.SUBSTITUTE, "대타 알림 수신",
+            NotificationConsentType.REPUTATION, "평판 알림 수신"
         );
     }
 }
