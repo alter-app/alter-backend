d57a443af3f98209af6bc0f4bbbaab0303482038
JunYoung Choi <juny0955@gmail.com>
2026-05-28 21:21:40 +0900

    feat: 대타 요청, 대타, 고정근무 비활성 추가

--- src/main/java/com/dreamteam/alter/application/workspace/usecase/ManagerResignWorkspaceWorker.java
+++ src/main/java/com/dreamteam/alter/application/workspace/usecase/ManagerResignWorkspaceWorker.java
@@ -3,12 +3,17 @@ package com.dreamteam.alter.application.workspace.usecase;
 import com.dreamteam.alter.common.exception.CustomException;
 import com.dreamteam.alter.common.exception.ErrorCode;
 import com.dreamteam.alter.domain.user.context.ManagerActor;
+import com.dreamteam.alter.domain.workspace.entity.SubstituteRequest;
+import com.dreamteam.alter.domain.workspace.entity.SubstituteRequestTarget;
 import com.dreamteam.alter.domain.workspace.entity.WorkspaceShift;
 import com.dreamteam.alter.domain.workspace.entity.WorkspaceWorker;
+import com.dreamteam.alter.domain.workspace.entity.WorkspaceWorkerSchedule;
 import com.dreamteam.alter.domain.workspace.port.inbound.ManagerResignWorkspaceWorkerUseCase;
+import com.dreamteam.alter.domain.workspace.port.outbound.SubstituteRequestQueryRepository;
 import com.dreamteam.alter.domain.workspace.port.outbound.WorkspaceQueryRepository;
 import com.dreamteam.alter.domain.workspace.port.outbound.WorkspaceShiftQueryRepository;
 import com.dreamteam.alter.domain.workspace.port.outbound.WorkspaceWorkerQueryRepository;
+import com.dreamteam.alter.domain.workspace.port.outbound.WorkspaceWorkerScheduleQueryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -24,6 +29,8 @@ public class ManagerResignWorkspaceWorker implements ManagerResignWorkspaceWorke
     private final WorkspaceQueryRepository workspaceQueryRepository;
     private final WorkspaceWorkerQueryRepository workspaceWorkerQueryRepository;
     private final WorkspaceShiftQueryRepository workspaceShiftQueryRepository;
+    private final WorkspaceWorkerScheduleQueryRepository workspaceWorkerScheduleQueryRepository;
+    private final SubstituteRequestQueryRepository substituteRequestQueryRepository;
 
     @Override
     public void execute(ManagerActor actor, Long workspaceId, Long workerId) {
@@ -38,8 +45,21 @@ public class ManagerResignWorkspaceWorker implements ManagerResignWorkspaceWorke
             throw new CustomException(ErrorCode.WORKSPACE_NOT_FOUND);
         }
 
-        List<WorkspaceShift> futureShifts = workspaceShiftQueryRepository.findFutureShiftsByAssignedWorker(worker, LocalDateTime.now());
+        List<WorkspaceShift> futureShifts = workspaceShiftQueryRepository
+            .findFutureShiftsByAssignedWorker(worker, LocalDateTime.now());
         futureShifts.forEach(WorkspaceShift::unassignWorker);
+
+        List<WorkspaceWorkerSchedule> fixedSchedules = workspaceWorkerScheduleQueryRepository
+            .getByWorkspaceWorker(worker);
+        fixedSchedules.forEach(WorkspaceWorkerSchedule::delete);
+
+        List<SubstituteRequest> activeRequests = substituteRequestQueryRepository
+            .findAllActiveByRequesterUserId(worker.getUser().getId());
+        activeRequests.forEach(SubstituteRequest::cancel);
+
+        List<SubstituteRequestTarget> pendingTargets = substituteRequestQueryRepository
+            .findAllPendingTargetsByUserId(worker.getUser().getId());
+        pendingTargets.forEach(SubstituteRequestTarget::expire);
 
         worker.resign();
     }
