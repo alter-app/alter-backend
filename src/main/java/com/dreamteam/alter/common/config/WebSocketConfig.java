6f4acf277480b3d216232270a32711bb3be4aee9
hodoon <ehgns79513@gmail.com>
2026-07-14 22:03:00 +0900

    refactor: [ALT-270] WebSocketConfig RabbitMQ relay 분기 제거, SimpleBroker 고정

--- src/main/java/com/dreamteam/alter/common/config/WebSocketConfig.java
+++ src/main/java/com/dreamteam/alter/common/config/WebSocketConfig.java
@@ -1,7 +1,6 @@
 package com.dreamteam.alter.common.config;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
@@ -16,30 +15,9 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     private final JwtChannelInterceptor jwtChannelInterceptor;
 
-    @Value("${chat.broker.relay.enabled:false}")
-    private boolean relayEnabled;
-    @Value("${chat.broker.relay.host:localhost}")
-    private String relayHost;
-    @Value("${chat.broker.relay.port:61613}")
-    private int relayPort;
-    @Value("${chat.broker.relay.client-login:guest}")
-    private String relayLogin;
-    @Value("${chat.broker.relay.client-passcode:guest}")
-    private String relayPasscode;
-
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
-        if (relayEnabled) {
-            config.enableStompBrokerRelay("/sub")
-                .setRelayHost(relayHost)
-                .setRelayPort(relayPort)
-                .setClientLogin(relayLogin)
-                .setClientPasscode(relayPasscode)
-                .setSystemLogin(relayLogin)
-                .setSystemPasscode(relayPasscode);
-        } else {
-            config.enableSimpleBroker("/sub");
-        }
+        config.enableSimpleBroker("/sub");
         config.setApplicationDestinationPrefixes("/pub");
     }
 
