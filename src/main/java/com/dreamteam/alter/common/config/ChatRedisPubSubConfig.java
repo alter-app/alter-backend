b4412e90f0617872ed5290dbb61d4505a5c516a6
hodoon <ehgns79513@gmail.com>
2026-07-14 21:57:44 +0900

    test: [ALT-270] 테스트 환경에서 Redis 리스너 컨테이너 비활성화(컨텍스트 부팅 Redis 의존 제거)

--- src/main/java/com/dreamteam/alter/common/config/ChatRedisPubSubConfig.java
+++ src/main/java/com/dreamteam/alter/common/config/ChatRedisPubSubConfig.java
@@ -3,6 +3,7 @@ package com.dreamteam.alter.common.config;
 import com.dreamteam.alter.adapter.outbound.chat.redis.ChatMessageRedisSubscriber;
 import com.dreamteam.alter.adapter.outbound.chat.redis.RedisChatMessageBroadcaster;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -16,6 +17,8 @@ public class ChatRedisPubSubConfig {
     private final ChatMessageRedisSubscriber chatMessageRedisSubscriber;
 
     @Bean
+    @ConditionalOnProperty(
+            name = "chat.redis.subscriber.enabled", havingValue = "true", matchIfMissing = true)
     public RedisMessageListenerContainer chatRedisMessageListenerContainer(RedisConnectionFactory connectionFactory) {
         RedisMessageListenerContainer container = new RedisMessageListenerContainer();
         container.setConnectionFactory(connectionFactory);
